a4d00aeb4872ce086a4a7c91b1f39f907baf800d
Rodrigo Fontanella <fontanellarodrigo13@gmail.com>
2021-05-17 20:15:08 -0300

    first commit

---
+++ app/src/main/java/com/example/trabcadastron1/ClienteActivity.java
@@ -0,0 +1,13 @@
+package com.example.trabcadastron1;
+import  android.app.Activity;
+import android.os.Bundle;
+
+public class ClienteActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.cadcliente);
+}
+
+
+}
